e41190f0ff520d372d1692a64bd1d8d13d1b83de
Raju Molla <61695603+raju-molla@users.noreply.github.com>
2020-10-05 11:24:38 +0600

    Add files via upload

---
+++ Codeforces/A. Floor Number.cpp.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+        if(a<=2){
+            cout<< "1"<<endl;
+        }
+        else {
+            int ans=(((a-3)/b)+2);
+            cout<<ans<<endl;
+        }
+
+    }
+}
